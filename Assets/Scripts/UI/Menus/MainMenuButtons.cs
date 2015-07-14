538a8093c4db75b1de637b89d822684e9772e56e
Gabriel Dias Lopes <ourprojectunity@gmail.com>
2015-07-13 22:08:32 -0300

    update no menu

---
+++ Assets/Scripts/UI/Menus/MainMenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    public void Webpage()
+    {
+        Application.OpenURL("URL DA PAGE");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
