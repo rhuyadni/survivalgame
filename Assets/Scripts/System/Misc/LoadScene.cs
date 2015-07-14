6cadf7f41c0c92e79cad9b83ee544b1032a36f94
Gabriel Dias Lopes <ourprojectunity@gmail.com>
2015-07-14 16:57:50 -0300

    first push

---
+++ Assets/Scripts/System/Misc/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+    public string SceneName = "";
+    public float loadSceneDelay = 1.5f;
+
+    void Start () {
+        Invoke("CallScene", loadSceneDelay);
+	}
+
+    void CallScene()
+    {
+        Application.LoadLevel(SceneName);
+    }
+}
---
+++ Assets/Scripts/UI/Menus/MainMenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    public void Webpage()
+    {
+        Application.OpenURL("URL DA PAGE");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        Application.LoadLevel(SysConfig.NewGameSceneName);
+    }
+
+}
