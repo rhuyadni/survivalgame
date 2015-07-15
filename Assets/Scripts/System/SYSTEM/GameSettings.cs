152c62f1018f21e00238e35a817fbf855c8abca4
Gabriel Dias Lopes <ourprojectunity@gmail.com>
2015-07-15 19:12:55 -0300

    *player spawn system in testscene

---
+++ Assets/Scripts/System/SYSTEM/GameSettings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameSettings : MonoBehaviour {
+
+    public Vector3 StartPosition = new Vector3(0, 0, 0);
+    public GameObject PlayerPrefab;
+
+	void Start () {
+        if (StartPosition == Vector3.zero)
+            Debug.LogError("Please define a StartPosition in GameSettings gameObject");
+
+        if (PlayerPrefab == null)
+            Debug.LogError("Please assign a CharacterPrefab in GameSettings gameObject");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/System/SYSTEM/SysConfig.cs
+++ Assets/Scripts/System/SYSTEM/SysConfig.cs
@@ -11,14 +11,20 @@ public class SysConfig : MonoBehaviour
     public const string LoadGameSceneName = "LoadGame";
     #endregion
 
-    #region MainVariables
+    #region Start Values
     public const int StartHealth = 1000;
     public const int StartThirsty = 500;
     public const int StartHungry = 500;
     public const int StartStamina = 300;
+    public const int StartLevel = 1;
+    public const int StartExperience = 1;
+    public const int NewLevelExp = 300;
     #endregion
 
     #region Modifiers
-
+    public const float HealthPerLevel = 1.2f;
+    public const float ThirstyPerLevel = 1.1f;
+    public const float HungryPerLevel = 1.1f;
+    public const float StaminaPerLevel = 1.1f;
     #endregion
 }
