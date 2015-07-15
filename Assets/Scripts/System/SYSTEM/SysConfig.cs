6c1be51fae02cd8ed936cf1b889c274c0c786587
Gabriel Dias Lopes <ourprojectunity@gmail.com>
2015-07-15 18:55:45 -0300

    *nothing important

--- Assets/Scripts/System/SYSTEM/SysConfig.cs
+++ Assets/Scripts/System/SYSTEM/SysConfig.cs
@@ -9,6 +9,16 @@ public class SysConfig : MonoBehaviour
     public const string MainMenuSceneName = "MainMenu";
     public const string NewGameSceneName = "LoadingNewGame";
     public const string LoadGameSceneName = "LoadGame";
+    #endregion
+
+    #region MainVariables
+    public const int StartHealth = 1000;
+    public const int StartThirsty = 500;
+    public const int StartHungry = 500;
+    public const int StartStamina = 300;
+    #endregion
+
+    #region Modifiers
 
     #endregion
 }
